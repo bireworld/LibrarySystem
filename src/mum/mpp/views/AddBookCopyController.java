0c1d20bcde03d06ae55805a9d20be22263005176
tekbale <tek.b.ale@gmail.com>
2015-12-03 17:20:22 -0600

    temp

--- src/mum/mpp/views/AddBookCopyController.java
+++ src/mum/mpp/views/AddBookCopyController.java
@@ -1,9 +1,53 @@
 package mum.mpp.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import mum.mpp.tay.backendinterface.AdminInterface;
 
 public class AddBookCopyController {
+	@FXML
+	private TextField txtfSearchBox;
+	
+	@FXML
+	private RadioButton rbtnByTitle;
+	
+	@FXML
+	private RadioButton rbtnByISBN;
+	
+	@FXML
+	private Button btnSearch;
+	
+	@FXML
+	private TableView tblvBookSearch;
+	
+	@FXML
+	private TableColumn tblcISBN;
+	
+	@FXML
+	private TableColumn tblcBookTitle;
+	
+	@FXML
+	private TableColumn tblcNumBookCopy;
+	
+	@FXML
+	private TableColumn tblcMaxCheckoutDuration;
+	
+	
+	private ToggleGroup searchToggleGroup;
+
 	private AdminInterface adminInterface;
+	
+	@FXML
+	public void initialize() {
+		//initRadioButtons();
+	}
+	
+	
 
 	public AdminInterface getAdminInterface() {
 		return adminInterface;
